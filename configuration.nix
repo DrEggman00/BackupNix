e0f1b30a3020a9ac428c484a8d008175f0287908
Dr. Eggman <vinicius.daniel@uft.edu.br>
2025-12-09 19:45:29 -0300

    update 12-09c

--- configuration.nix
+++ configuration.nix
@@ -26,31 +26,6 @@
   #   keyMap = "us";
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-   services.pipewire = {
-     enable = true;
-     pulse.enable = true;
-   };
-
   
   # List services that you want to enable:
   # Copy the NixOS configuration file and link it from the resulting system
--- modules/services.nix
+++ modules/services.nix
@@ -7,4 +7,31 @@
     services.libinput.enable = true;
     # Enable the OpenSSH daemon.
     # services.openssh.enable = true;
+
+    hardware.bluetooth.enable = true;
+
+    # Configure keymap in X11
+    # services.xserver.xkb.layout = "us";
+    # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+    # Enable CUPS to print documents.
+    # services.printing.enable = true;
+
+    # Enable sound.
+    # services.pulseaudio.enable = true;
+    # OR
+    services.pipewire = {
+        enable = true;
+        pulse.enable = true;
+    };
+
+    # Enable the X11 windowing system.
+    # services.xserver.enable = true;
+    services.xserver.enable = true;
+    services.displayManager.sddm.enable = true;
+    services.displayManager.sddm.wayland.enable = true;
+    services.desktopManager.plasma6.enable = true;
+
+  
+
 }
