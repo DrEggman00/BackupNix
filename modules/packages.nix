313b67f4fee8fb36adc4a76a4189b51bdce2effc
Dr. Eggman <vinicius.daniel@uft.edu.br>
2025-12-10 10:42:02 -0300

    update 12-10

--- modules/packages.nix
+++ modules/packages.nix
@@ -12,6 +12,7 @@
         vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
         wget
         firefox
+	sddm-astronaut
         nano
         kdePackages.konsole
         htop
@@ -31,5 +32,27 @@
         clang
         nodejs
         gnumake
+	ncdu
    ];
+	
+   #virtualisation.virtualbox.host.enable = true;
+   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+   nixpkgs.config.allowUnfree = true;
+   virtualisation.virtualbox.host.enable = true;
+   virtualisation.virtualbox.host.enableExtensionPack = true;
+   virtualisation.virtualbox.guest.enable = true;
+   virtualisation.virtualbox.guest.dragAndDrop = true;
+
+   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    	"steam"
+   	 "steam-unwrapped"
+   ];
+
+
+   programs.steam = {
+  		enable = true;
+  		remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  		dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+	};
+
 }
